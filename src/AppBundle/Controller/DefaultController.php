7f1a1d7013921eb7e4606faa634df201e15b1078
cezbil <czaro93pl@gmail.com>
2019-11-26 19:46:42 +0000

    fizzbuzz done

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,20 +13,54 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        // symfony example code
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
 
     /**
-     * @Route("/fizzBuzz", name="homepage")
+     * @Route("/fizzBuzz", name="fizzbuzz")
      */
     public function fizzBuzz(Request $request)
     {
-        // replace this example code with whatever you need
+        $fizzBuzz = $this->generateFizzBuzzArray();
+        // render view for fizzbuzz
         return $this->render('fizzBuzz/fizzBuzz.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'fizzBuzzArray' => $fizzBuzz,
         ]);
+    }
+    /**
+     * @return array
+     */
+    private function generateFizzBuzzArray($counter = 20) {
+        $fizzBuzzArray = [];
+        for ($i = 1; $i <= $counter; $i++) {
+            $fizz =  (0 === $i % 3);
+            $buzz =  (0 === $i % 5);
+
+            if (!$fizz && !$buzz) {
+                array_push($fizzBuzzArray, $i);
+                continue;
+            }
+
+            if ($fizz && !$buzz) {
+                array_push($fizzBuzzArray, 'Fizz');
+                continue;
+            }
+
+            if ($buzz && !$fizz) {
+                array_push($fizzBuzzArray, 'Buzz');
+                continue;
+            }
+
+            if ($fizz && $buzz) {
+                array_push($fizzBuzzArray, 'FizzBuzz');
+                continue;
+            }
+        }
+        return $fizzBuzzArray;
+
     }
 }
